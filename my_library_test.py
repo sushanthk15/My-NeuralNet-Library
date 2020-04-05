cc26bbdd9991b3f67f4440357ad1e08c677c23f5
Sushanth Keshav <sk66vode@imfd7070.imfd.tu-freiberg.de>
2020-04-05 03:42:19 +0200

    Updated input file for user

--- my_library_test.py
+++ my_library_test.py
@@ -11,15 +11,15 @@ from neural_net_library import my_neural_network
 import time
 
 #The input for hidden_layer_dims must be a list containing number of neurons in each hiden layer
-hidden_layer_dims = [5,5]
+hidden_layer_dims = [10,10,10]
 print('The Hidden Layer Dimensions : ',hidden_layer_dims)
 
 #Enter the file name. Has to be a string with extension.
-file_name = 'BostonDataSet.txt'
+file_name = 'output_bigoni_martin_ellipse.txt'
 print("The file name is : ", file_name)
 
 #Enter the hyper-parameters. Note that all the hyper-parameters are Non-Negative
-epochs = 10001
+epochs = 100
 lr = 0.01
 # Mandatory for Momentum and Adam range: [0,1)
 b1 = 0.9 
@@ -41,7 +41,7 @@ start_time = time.clock()
 model = my_neural_network(hidden_layer_dims)
 model.load_dataset(file_name)
 model.test_train_split()
-model.NN_model(epochs, lr,beta1=b1,beta2=b2,activation_function = activate, batching = False, batch_size= 128,optimizer = optim,early_stop=True)
+model.NN_model(epochs, lr,beta1=b1,beta2=b2,activation_function = activate,regularization = 0.01, batching = False, batch_size= 128,optimizer = optim,error_method = 'MSE',early_stop=True,tolerance=1e-4,learning_rate_decay=0.5,print_cost = False,plot=True)
 
 #Stop timer
 stop_time = time.clock()
@@ -53,3 +53,5 @@ model.evaluate()
 total_time = stop_time - start_time
 print("The total time taken for learning is : %.2f seconds" %(total_time))
 
+
+           
