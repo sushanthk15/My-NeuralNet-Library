e1042f484f9cceb432e901b4a69e453524b78bfd
sushanthk15 <k.sushanth.k@gmail.com>
2020-04-05 17:57:54 +0200

    Test file

--- my_library_test_check.py
+++ my_library_test_check.py
@@ -1,30 +1,57 @@
-#!/usr/bin/env python
-# coding: utf-8
+#####################################################################
+# Name :  Sushanth Keshav | Matrikel Nr: 63944
+# Topic : Implementing the learning process through my_neural_network
+# Programming Language : Python
+# Last Updated : 04.04.2020
+#####################################################################
 
-# ## Loading the Libraries
-
-# In[ ]:
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-from neural_net_library import *
+import numpy as np 
+import matplotlib.pyplot as plt 
 from neural_net_library import my_neural_network
+import time
 
-
-# ## Training the Model
-
-# In[ ]:
-
-
+#The input for hidden_layer_dims must be a list containing number of neurons in each hiden layer
 hidden_layer_dims = [10,10,10]
+print('The Hidden Layer Dimensions : ',hidden_layer_dims)
+
+#Enter the file name. Has to be a string with extension.
+file_name = 'Bigoni_yield_data.txt'
+print("The file name is : ", file_name)
+
+#Test file name
+file_namet= "output_bigoni_ellipse_check1.txt"
+
+#Enter the hyper-parameters. Note that all the hyper-parameters are Non-Negative
+epochs = 10000
+lr = 0.01
+# Mandatory for Momentum and Adam range: [0,1)
+b1 = 0.9 
+# Mandatory for RMSProp and Adam range: [0,1)
+b2 = 0.999
+activate = 'sigmoid'
+optim = 'Momentum'
+
+
+
+print("The learning rate is {}, beta1 = {}, beta2 = {}:".format(lr,b1,b2))
+print("The activation function is :", activate)
+print("The Optimizer function is :", optim)
+
+#Start Timer
+start_time = time.clock()
+
+#Execution of the Neural Network Model
 model = my_neural_network(hidden_layer_dims)
-file_name = 'output_bigoni_martin_ellipse.txt'
 model.load_dataset(file_name)
 model.test_train_split()
-optim = 'Momentum'
-model.NN_model(10000, 0.01,beta1=0.9,beta2=0.999,regularization = 0.7,activation_function = "sigmoid", batching = False, batch_size= 128,optimizer = optim,early_stop=False,learning_rate_decay=0.5)
+model.NN_model(epochs, lr,beta1=b1,beta2=b2,activation_function = activate,regularization = 0.01, batching = False, batch_size= 128,optimizer = optim,error_method = 'MSE',early_stop=True,tolerance=1e-4,learning_rate_decay=0.5,print_cost = False,plot=True)
 
+#Stop timer
+stop_time = time.clock()
+
+#Time Elapsed
+total_time = stop_time - start_time
+print("The total time taken for learning is : %.2f seconds" %(total_time))
 
 ### Testing the Model
 
@@ -33,7 +60,6 @@ model.NN_model(10000, 0.01,beta1=0.9,beta2=0.999,regularization = 0.7,activation
 
 #Load the unseen data or the testing data
 model_t = my_neural_network(hidden_layer_dims)
-file_namet= "output_bigoni_ellipse_check1.txt"
 model_t.load_dataset(file_namet)
 
 #Retrieve the Normalized inputs and outputs
@@ -130,3 +156,4 @@ ax1.set_zlabel('q', fontsize=20)
 plt.legend(prop={"size":15})
 plt.show()
 
+           
